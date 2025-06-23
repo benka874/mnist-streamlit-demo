18fe78afd49cd0dd97c3b31bf25bc23dd1347b19
benka874 <143692047+benka874@users.noreply.github.com>
2025-06-23 20:43:38 +0300

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
-from tensorflow.keras.datasets import mnist
+from keras.datasets import mnist
 import random
 
 (x_train, y_train), (_, _) = mnist.load_data()
